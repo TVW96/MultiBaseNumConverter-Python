700f9cba1396980bf863f325074622859856399b
Troy Washington <troyvw96@gmail.com>
2024-10-13 15:30:56 -0700

    error handling in flask

--- app.py
+++ app.py
@@ -78,5 +78,10 @@ def convert():
     except ValueError as e:
         return jsonify({"error": str(e)}), 400
 
+@app.errorhandler(500)
+def internal_error(error):
+    return "An internal error occurred: {}".format(error), 500
+
+
 if __name__ == "__main__":
     app.run(debug=True)
